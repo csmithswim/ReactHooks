c14d82f5b4860f299aa5f3831839f1a43af1f58a
Cullen Smith <cullensmi@gmail.com>
2020-09-20 14:36:40 -0400

    intro to customhooks

---
+++ customHooks.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import useLocalStorage from './useLocalStorage';
+import useUpdateLogger from './useUpdateLogger';
+
+export default function App() { 
+    const [name, setName] = useLocalStorage('name', '')
+
+    return (
+        <input 
+        type='text'
+        value={name}
+        onChange={e => setName(e.target.value)}
+        />
+    )
+}
